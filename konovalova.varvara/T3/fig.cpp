0478431e5bb221bf9e60edd0dbdda4c1b93bf9ab
bazber <varvara.konovalova.06@mail.ru>
2024-04-28 14:02:04 +0300

    CG fix

--- konovalova.varvara/T3/fig.cpp
+++ konovalova.varvara/T3/fig.cpp
@@ -6,28 +6,27 @@
 
 using namespace std::placeholders;
 
-bool konovalova::Point::operator==(const Point &other) const
+bool konovalova::Point::operator==(const Point& other) const
 {
     return x == other.x && y == other.y;
 }
 
-bool konovalova::Point::operator!=(const Point &other) const
+bool konovalova::Point::operator!=(const Point& other) const
 {
     return x != other.x || y != other.y;
 }
 
-bool konovalova::Point::operator<(const Point &other) const
+bool konovalova::Point::operator<(const Point& other) const
 {
     if (x == other.x) return y < other.y;
     return x < other.x;
 }
 
-bool konovalova::Polygon::operator==(const Polygon &other) const
+bool konovalova::Polygon::operator==(const Polygon& other) const
 {
     if (points.size() != other.points.size()) return false;
-
     auto other_pnt = other.points.begin();
-    auto testFunc = [&other_pnt] (const Point &pnt)
+    auto testFunc = [&other_pnt] (const Point& pnt)
     {
         bool result = pnt == *other_pnt;
         other_pnt++;
@@ -36,7 +35,24 @@ bool konovalova::Polygon::operator==(const Polygon &other) const
     return std::all_of(points.begin(), points.end(), testFunc);
 }
 
-bool konovalova::Polygon::operator<(const Polygon &other) const
+bool konovalova::Polygon::operator<(const Polygon& other) const
 {
     return area() < other.area();
+}
+
+double konovalova::Polygon::area(void) const
+{
+    double res = 0;
+    auto prev = *points.begin();
+    auto countFunc = [&res, &prev] (int ac, const Point& pnt)
+    {
+        res = ac;
+        res += prev.x * pnt.y;
+        res -= pnt.x * prev.y;
+        prev = pnt;
+        return res;
+    };
+    std::accumulate(points.begin() + 1, points.end(), 0,
+    std::bind(countFunc, _1, _2));
+    return 0.5 * abs(res);
 }
